085cf92a5188cc642eec1327594cc9fd15919c9b
rexTOLA <ander.artola@opendeusto.es>
2015-11-16 11:13:27 +0100

    prueba

---
+++ pppp/src/Main/ObjetoClase.java
@@ -0,0 +1,41 @@
+package Main;
+
+import java.util.ArrayList;
+
+public class ObjetoClase {
+
+	/**
+	 * Codigo de la clase
+	 * @param args
+	 */
+	int cod_c;
+	
+	/**
+	 * Strings
+	 * @param args
+	 */
+	
+	/**
+	 * Strings
+	 * @param args
+	 */
+	
+	/**
+	 * Arraylist para guardar los nombres de los metodos
+	 * @param args
+	 */
+	ArrayList<String>metodos;
+	
+	/**
+	 * Arraylist para guardar los nombres de las variables
+	 * @param args
+	 */
+	ArrayList<String>variables;
+	
+	//Main de prueba de clase
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
