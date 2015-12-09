c7c5fc58bb3f665bc77a0aed5aa7346b0578f6e0
Itsazainbilbao <Itsazainbilbao@gmail.com>
2015-12-09 09:15:07 +0100

    Segundo commit

--- pppp/src/Capa_de_Presentacion/VentanaJuego.java
+++ pppp/src/Capa_de_Presentacion/VentanaJuego.java
@@ -1,96 +1,55 @@
 package Capa_de_Presentacion;
 
-
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
+import java.awt.Frame;
+import java.awt.event.*;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.text.DefaultCaret;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
-import javax.swing.JEditorPane;
-import javax.swing.JTextField;
-import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JTextField;
 
 public class VentanaJuego extends JFrame {
+	protected static VentanaJuego miVentana = null;
+	private boolean lineaLeida = false;
+	protected JPanel contentPane;
+	protected static JTextField tfLineaEntrada = new JTextField();
+	protected JTextArea textArea = new JTextArea();
+	protected JScrollPane scrollPane = new JScrollPane(textArea);
+	protected JTextArea taEnunciado = new JTextArea();
+	protected JScrollPane spEnunciado = new JScrollPane(taEnunciado);
+	protected JTextArea taSalida = new JTextArea();
+	protected JScrollPane spSalida = new JScrollPane(taSalida);
 
-	private JPanel contentPane;
-	private JTextField textField;
+
+
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
+		/*EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					VentanaJuego frame = new VentanaJuego();
 					frame.setVisible(true);
-				} catch (Exception e) {
+					} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
-		});
+		});*/
+		VentanaJuego.init();
+		VentanaJuego.println("hola");
+		
+
 	}
 
-	/** Cierra la ventana de consola.
-	 */
-	static public void finish() {
-		
-	}
-	/** Lee una línea desde la línea de entrada de la ventana
-	 * @return	Línea leída
-	 */
-	static public String leerLinea() {
-		
-	}
-	/** Escribe en la ventana una línea
-	 * @param s	String a visualizar en la ventana
-	 */
-	static public void println( String s ) {
-		
-	}
-	/** Comprueba si el ejercicio está bien
-	 */
-	static public void check() {
-		
-	}
-	/** crea un nivel nuevo y vacío
-	 */
-	static public void crear_Nivel() {
-		
-	}
-	/** crea una clase nueva nueva en blanco
-	 */
-	static public void crear_Clase() {
-		
-	}
-	/** Imprime por consola una lista de los comandos accesibles
-	 */
-	static public void help() {
-		
-	}
-	/** Imprime por consola todos los comandos relacionados por la palabra clave
-	 * @param String key palabra clave a buscar
-	 */
-	static public void help(String key) {
-		
-	}
-	/** busca en el directorio marcado todas las carpetas y ficheros que hay y los muestra por pantalla
-	 * @param String dir dirección que se introduce
-	 */
-	static public void search(String dir) {
-		
-	}
-	
-	
-	
-	
-	
-	
-	
-	
 	/**
 	 * Create the frame.
 	 */
@@ -100,43 +59,94 @@ public class VentanaJuego extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		
-		JEditorPane editorPane = new JEditorPane();
-		
-		textField = new JTextField();
-		textField.setColumns(10);
-		
-		JTextArea textArea = new JTextArea();
-		textArea.setEditable(false);
-		
-		JTextArea textArea_1 = new JTextArea();
-		textArea_1.setEditable(false);
+
+		tfLineaEntrada = new JTextField();
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
-					.addComponent(editorPane, GroupLayout.PREFERRED_SIZE, 401, GroupLayout.PREFERRED_SIZE)
-					.addGap(18)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-						.addComponent(textArea_1, GroupLayout.DEFAULT_SIZE, 345, Short.MAX_VALUE)
-						.addComponent(textArea, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 345, Short.MAX_VALUE)
-						.addComponent(textField, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 345, Short.MAX_VALUE))
-					.addContainerGap())
-		);
+						.addGap(12)
+						.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 410, GroupLayout.PREFERRED_SIZE)
+						.addGap(18)
+						.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(spSalida, GroupLayout.DEFAULT_SIZE, 324, Short.MAX_VALUE)
+								.addComponent(spEnunciado, GroupLayout.DEFAULT_SIZE, 324, Short.MAX_VALUE)
+								.addComponent(tfLineaEntrada, GroupLayout.DEFAULT_SIZE, 324, Short.MAX_VALUE))
+						.addContainerGap())
+				);
 		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
-						.addComponent(editorPane, GroupLayout.PREFERRED_SIZE, 498, GroupLayout.PREFERRED_SIZE)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(textArea_1, GroupLayout.PREFERRED_SIZE, 186, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(textArea)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
-					.addContainerGap(42, Short.MAX_VALUE))
-		);
+						.addContainerGap()
+						.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+								.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 503, GroupLayout.PREFERRED_SIZE)
+								.addGroup(gl_contentPane.createSequentialGroup()
+										.addComponent(spEnunciado, GroupLayout.PREFERRED_SIZE, 205, GroupLayout.PREFERRED_SIZE)
+										.addPreferredGap(ComponentPlacement.RELATED)
+										.addComponent(spSalida, GroupLayout.PREFERRED_SIZE, 265, GroupLayout.PREFERRED_SIZE)
+										.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+										.addComponent(tfLineaEntrada, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+						.addContainerGap(37, Short.MAX_VALUE))
+				);
+
+
+		taSalida.setEditable(false);
+		taEnunciado.setEditable(false);
 		contentPane.setLayout(gl_contentPane);
+		// Política de visualización de caret -cursor en textarea-
+				DefaultCaret caret = (DefaultCaret) taSalida.getCaret();
+				caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);       
+				// Escuchadores
+				tfLineaEntrada.addActionListener( new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent arg0) {
+						lineaLeida = true;
+					}
+				});
+				
+	}
+	
+
+	/** Escribe en la ventana de la consola una línea
+	 * @param s	String a visualizar en la ventana
+	 */
+	public static void println( String s ) {
+		if (miVentana == null) init();
+		miVentana.taSalida.append( s + "\n" );
+	}
+	/** Escribe en la ventana del enunciado una línea
+	 * @param s	String a visualizar en la ventana
+	 */
+	public static void printEnunciado( String s ) {
+		if (miVentana == null) init();
+		miVentana.taEnunciado.append( s + "\n" );
+	}
+	/** Lee una línea desde la línea de entrada de la ventana
+	 * @return	Línea leída
+	 */
+	static public String readLine() {
+		if (miVentana == null) init();
+		while (!miVentana.lineaLeida) {
+			// Espera hasta que se lea algo
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) { }
+		}
+		miVentana.lineaLeida = false;
+		String ret  = miVentana.tfLineaEntrada.getText();
+		miVentana.tfLineaEntrada.setText( "" );
+		return ret;
+	}
+	static public void finish(){
+		miVentana.dispose();
+	}
+	static public void init() {
+
+		miVentana = new VentanaJuego();
+		miVentana.setVisible( true );
+		miVentana.tfLineaEntrada.requestFocus();
+		
+		Consola con=new Consola();
+						
 	}
 }
