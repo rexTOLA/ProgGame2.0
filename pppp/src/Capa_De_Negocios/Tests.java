678a5beced6924187c136fb50b8ef4978c2dfb30
Ainhoa <Ainhoa@Ainhoa-PC>
2015-12-19 17:18:35 +0100

    Clase Tests

--- pppp/src/Capa_De_Negocios/Tests.java
+++ pppp/src/Capa_De_Negocios/Tests.java
@@ -8,7 +8,22 @@ import java.io.InputStreamReader;
 public class Tests {
 	
 	
-    public static void main(String[] args) throws IOException { 
+    public static void main(String[] args) {
+    	//Ejercicio deslizar
+    	try {
+			if(ejercicioDeslizar()){
+				//ejercicio superado
+			}
+			else{
+				//ejercicio no superado
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+	
+
+    public static boolean ejercicioDeslizar() throws IOException{
     	System.out.print("Move your character(X) to get to the exit (*). To move, type: Up: u; Down: d; Left: l; Right: r. And be carefull with the ice, you'll slide!!\n");
     	Ej_Deslizar ed = new Ej_Deslizar();
     	String s;
@@ -20,7 +35,7 @@ public class Tests {
 		while(!ed.move(s));	
     	
     	System.out.println("Congratulations!!! You won!!!");
+    	return true;
     }
-	
-
+    
 }
