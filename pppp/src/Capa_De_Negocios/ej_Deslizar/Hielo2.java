dd2877385d8967f13e5894972ac2a1e409f2a57c
Ainhoa <Ainhoa@Ainhoa-PC>
2015-12-19 17:35:52 +0100

    asdf

--- pppp/src/Capa_De_Negocios/ej_Deslizar/Hielo2.java
+++ pppp/src/Capa_De_Negocios/ej_Deslizar/Hielo2.java
@@ -3,11 +3,11 @@ package Capa_De_Negocios.ej_Deslizar;
 public class Hielo2 extends Hielo{
 
 	public Hielo2(){
-		super('h');
+		super('s');
 	}
 	
 	//Método que tendría que añadir el usuario
-//	public int stop_slide() {
-//		return STOP;
-//	}
+	public int stop_slide() {
+		return STOP;
+	}
 }
